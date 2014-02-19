po1/key_teleop

Add SimpleKeyTeleop, with simpler keyboard controls
Some of the code could be modularized from KeyTeleop, but
as small modifications had to be made here and there, almost
everything had to be touched anyway.

Enabled as a new default.
TODO: add a runtime way to change the speed

scripts/key_teleop.py
@@ -10,7 +10,6 @@
 import roslib
 roslib.load_manifest('key_teleop')
 
-from threading import Thread
 import curses
 import math
 
@@ -72,7 +71,7 @@ class TextWindow():
             text = text.ljust(width)
             self._screen.addstr(y, x, text)
             y += 1
-    
+
     def refresh(self):
         self._screen.refresh()
 
@@ -178,9 +177,86 @@ class KeyTeleop():
         twist = self._get_twist(self._linear, self._angular)
         self._pub_cmd.publish(twist)
 
+
+class SimpleKeyTeleop():
+    def __init__(self, interface):
+        self._interface = interface
+        self._pub_cmd = rospy.Publisher('key_vel', Twist)
+
+        self._hz = rospy.get_param('~hz', 10)
+
+        self.forward_rate = rospy.get_param('~forward_rate', 0.8)
+        self.backward_rate = rospy.get_param('~backward_rate', 0.5)
+        self.rotation_rate = rospy.get_param('~rotation_rate', 1.0)
+        self.last_pressed = {}
+        self.angular = 0
+        self.linear = 0
+
+    movement_bindings = {
+        curses.KEY_UP:    ( 1,  0),
+        curses.KEY_DOWN:  (-1,  0),
+        curses.KEY_LEFT:  ( 0,  1),
+        curses.KEY_RIGHT: ( 0, -1),
+    }
+
+    def run(self):
+        rate = rospy.Rate(self._hz)
+        while True:
+            while True:
+                keycode = self._interface.read_key()
+                if keycode is None:
+                    break
+                self._key_pressed(keycode)
+            self._set_velocity()
+            self._publish()
+            rate.sleep()
+
+    def _get_twist(self, linear, angular):
+        twist = Twist()
+        twist.linear.x = linear
+        twist.angular.z = angular
+        return twist
+
+    def _set_velocity(self):
+        now = rospy.get_time()
+        keys = []
+        for a in self.last_pressed:
+            if now - self.last_pressed[a] < 0.4:
+                keys.append(a)
+        linear = 0.0
+        angular = 0.0
+        for k in keys:
+            l, a = self.movement_bindings[k]
+            linear += l
+            angular += a
+        if linear > 0:
+            linear = linear * self.forward_rate
+        else:
+            linear = linear * self.backward_rate
+        angular = angular * self.rotation_rate
+        self._angular = angular
+        self._linear = linear
+
+    def _key_pressed(self, keycode):
+        now = rospy.get_time()
+        if keycode == ord('q'):
+            rospy.signal_shutdown('Bye')
+        elif keycode in self.movement_bindings:
+            self.last_pressed[keycode] = now
+
+    def _publish(self):
+        self.interface.clear()
+        self.interface.write_line(2, 'Linear: %d, Angular: %d' % (self.linear, self.angular))
+        self.interface.write_line(5, 'Use arrow keys to move, space to stop, q to exit.')
+        self.interface.refresh()
+
+        twist = self._get_twist(self.linear, self.angular)
+        self._pub_cmd.publish(twist)
+
+
 def main(stdscr):
     rospy.init_node('key_teleop')
-    app = KeyTeleop(TextWindow(stdscr))
+    app = SimpleKeyTeleop(TextWindow(stdscr))
     app.run()
 
 if __name__ == '__main__':

ros-teleop/teleop_tools

scripts/key_teleop.py
@@ -10,7 +10,6 @@
 import roslib
 roslib.load_manifest('key_teleop')
 
-from threading import Thread
 import curses
 import math
 
@@ -72,7 +71,7 @@ class TextWindow():
             text = text.ljust(width)
             self._screen.addstr(y, x, text)
             y += 1
-    
+
     def refresh(self):
         self._screen.refresh()
 
@@ -178,9 +177,86 @@ class KeyTeleop():
         twist = self._get_twist(self._linear, self._angular)
         self._pub_cmd.publish(twist)
 
+
+class SimpleKeyTeleop():
+    def __init__(self, interface):
+        self._interface = interface
+        self._pub_cmd = rospy.Publisher('key_vel', Twist)
+
+        self._hz = rospy.get_param('~hz', 10)
+
+        self._forward_rate = rospy.get_param('~forward_rate', 0.8)
+        self._backward_rate = rospy.get_param('~backward_rate', 0.5)
+        self._rotation_rate = rospy.get_param('~rotation_rate', 1.0)
+        self._last_pressed = {}
+        self._angular = 0
+        self._linear = 0
+
+    movement_bindings = {
+        curses.KEY_UP:    ( 1,  0),
+        curses.KEY_DOWN:  (-1,  0),
+        curses.KEY_LEFT:  ( 0,  1),
+        curses.KEY_RIGHT: ( 0, -1),
+    }
+
+    def run(self):
+        rate = rospy.Rate(self._hz)
+        while True:
+            while True:
+                keycode = self._interface.read_key()
+                if keycode is None:
+                    break
+                self._key_pressed(keycode)
+            self._set_velocity()
+            self._publish()
+            rate.sleep()
+
+    def _get_twist(self, linear, angular):
+        twist = Twist()
+        twist.linear.x = linear
+        twist.angular.z = angular
+        return twist
+
+    def _set_velocity(self):
+        now = rospy.get_time()
+        keys = []
+        for a in self._last_pressed:
+            if now - self._last_pressed[a] < 0.4:
+                keys.append(a)
+        linear = 0.0
+        angular = 0.0
+        for k in keys:
+            l, a = self.movement_bindings[k]
+            linear += l
+            angular += a
+        if linear > 0:
+            linear = linear * self._forward_rate
+        else:
+            linear = linear * self._backward_rate
+        angular = angular * self._rotation_rate
+        self._angular = angular
+        self._linear = linear
+
+    def _key_pressed(self, keycode):
+        now = rospy.get_time()
+        if keycode == ord('q'):
+            rospy.signal_shutdown('Bye')
+        elif keycode in self.movement_bindings:
+            self._last_pressed[keycode] = now
+
+    def _publish(self):
+        self._interface.clear()
+        self._interface.write_line(2, 'Linear: %d, Angular: %d' % (self._linear, self._angular))
+        self._interface.write_line(5, 'Use arrow keys to move, space to stop, q to exit.')
+        self._interface.refresh()
+
+        twist = self._get_twist(self._linear, self._angular)
+        self._pub_cmd.publish(twist)
+
+
 def main(stdscr):
     rospy.init_node('key_teleop')
-    app = KeyTeleop(TextWindow(stdscr))
+    app = SimpleKeyTeleop(TextWindow(stdscr))
     app.run()
 
 if __name__ == '__main__':
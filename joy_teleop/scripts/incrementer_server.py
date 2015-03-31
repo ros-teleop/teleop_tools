5a11bf7213f35ae31353fc696f31266f8b7e8fde
Bence Magyar <bence.magyar@pal-robotics.com>
2015-03-31 16:54:38 +0200

    Replace query_state with wait_for_message on state topic Name refactoring

--- joy_teleop/scripts/incrementer_server.py
+++ joy_teleop/scripts/incrementer_server.py
@@ -1,23 +1,19 @@
 #!/usr/bin/env python
 import rospy
 import actionlib
-import trajectory_msgs.msg as TJM
-from control_msgs.srv import QueryTrajectoryState as QTS
-import teleop_tools_msgs.msg as JTA
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from control_msgs.msg import JointTrajectoryControllerState as JTCS
+from teleop_tools_msgs.msg import IncrementAction as TTIA
 
 class IncrementerServer:
     def __init__(self, controller_ns):
         self._as = actionlib.SimpleActionServer("increment",
-            JTA.IncrementAction, execute_cb=self._as_cb, auto_start=False)
-        self._command_pub = rospy.Publisher("command", TJM.JointTrajectory, queue_size=2)
-        #TODO add timeout
-        self._service_client = rospy.ServiceProxy("query_state", QTS, True)
-        rospy.sleep(2.0)
-        self._service_client.wait_for_service()
-        result = self._service_client.call(rospy.Time.now())
-        [self._value] = result.position
-        self._goal = TJM.JointTrajectory()
-        self._goal.joint_names = result.name
+            TTIA, execute_cb=self._as_cb, auto_start=False)
+        self._command_pub = rospy.Publisher("command", JointTrajectory, queue_size=1)
+        state = rospy.wait_for_message("state", JTCS)
+        [self._value] = state.actual.positions
+        self._goal = JointTrajectory()
+        self._goal.joint_names = state.joint_names
         rospy.loginfo('Connected to ' + controller_ns)
         self._as.start()
 
@@ -26,13 +22,11 @@ class IncrementerServer:
         self._as.set_succeeded([])
 
     def increment_by(self, increment):
-        #cancel all goals sent to server
-        #TODO validate these values before sending to avoid increasing any well above the limit
-        result = self._service_client.call(rospy.Time.now())
-        [self._value] = result.position
+        state = rospy.wait_for_message("state", JTCS)
+        [self._value] = state.actual.positions
         self._value += increment
         rospy.loginfo('Sent goal of ' + str(self._value))
-        point = TJM.JointTrajectoryPoint()
+        point = JointTrajectoryPoint()
         point.positions = [self._value]
         point.time_from_start = rospy.Duration(0.1)
         self._goal.points = [point]
